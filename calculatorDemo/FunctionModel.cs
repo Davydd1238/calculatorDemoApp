df5f178585ce775211680ee4be653d7b9646404c
Josh <jblau2017@yahoo.com>
2025-08-02 06:51:32 -0600

    Add project files.

---
+++ calculatorDemo/FunctionModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculatorDemo
+{
+    public class FunctionModel
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Divide(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by 0 ");
+            }
+            return num1 / num2;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ calculatorDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace calculatorDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionModel calculation = new FunctionModel();
+
+            Console.WriteLine("Welcome to the Calculator Demo by Josh Blau");
+                        
+            Console.WriteLine("What function would you like to compute:");
+            string function = Console.ReadLine();
+            
+            
+            Console.WriteLine("What is the first number:");
+            string firstMumber = Console.ReadLine();
+            int num1 = int.Parse(firstMumber);
+
+            
+            Console.WriteLine("What is the second number:");
+            string secondMumber = Console.ReadLine();
+            int num2 = int.Parse(secondMumber);
+
+            
+            //Console.WriteLine("Would you like to add another number to the equation? ( yes /no)");
+            //string additionalNumber = Console.ReadLine();
+
+            Console.WriteLine($"The total is . ");
+            Console.ReadLine();
+
+        }
+    }
+}
